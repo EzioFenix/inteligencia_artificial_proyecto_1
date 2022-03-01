0093c31ca5cee05b9a10cec3593b110aa9c67e4f
BaudolinoII <30911202+BaudolinoII@users.noreply.github.com>
2022-03-01 16:28:04 -0600

    Certificacion
    Se ha logrado compilar con un editor de python

--- Codigo/clases/ciudad.py
+++ Codigo/clases/ciudad.py
@@ -1,5 +1,7 @@
+from .ruta import ruta
+
 class ciudad:
-    nombre = ""
+    nombre = 'city'
     optPath = None # Ruta optima
     destinos = list() # ciudades vecinas a las que estan 
     costo = list() # Costo por el destino, asociado a la lista destinos
@@ -53,28 +55,27 @@ class ciudad:
      #Se requiere de comprobación en grupo
         
     def GeneraRutas(self,lista):
-	if (self.optPath == NONE):
-		self.optPath = Ruta()
-		self.optPath.recorrido.append(self)
-	n = 0
-	for destino in self.Destinos:
-		noVisto = True
-		for camino in self.optPath.recorrido:
-			noVisto = noVisto and (destino != camino)
-		if (noVisto):
-			hijo = Ruta()
-			hijo.recorrido = self.optPath
-			hijo.pronderacion = self.precios.get(n) + self.optPath.ponderacion
-			hijo.recorrido.append(destino)
-			lista.append(hijo)
-		n+=1
+    	if (self.optPath == None):
+    		self.optPath = ruta()
+    		self.optPath.recorrido.append(self)
+    	n = 0
+    	for destino in self.Destinos:
+    		noVisto = True
+    		for camino in self.optPath.recorrido:
+    			noVisto = noVisto and (destino != camino)
+    		if (noVisto):
+    			hijo = ruta()
+    			hijo.recorrido = self.optPath
+    			hijo.pronderacion = self.precios.get(n) + self.optPath.ponderacion
+    			hijo.recorrido.append(destino)
+    			lista.append(hijo)
+    		n+=1
 
 def InsertarRuta(self, ruta):
-	if (self.optPath == NONE):
+	if (self.optPath == None):
 		self.optPath = ruta
-		return NONE
-	if(self.optPath.ponderacion > ruta.ponderacion)
+		return None
+	if(self.optPath.ponderacion > ruta.ponderacion):
 		aux = self.optPath
 		self.optPath = ruta
 		return aux
-	return ruta
