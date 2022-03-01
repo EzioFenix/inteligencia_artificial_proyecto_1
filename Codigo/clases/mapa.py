fc7509269f21a74322cd6884a06b2f793d9b268a
BaudolinoII <30911202+BaudolinoII@users.noreply.github.com>
2022-03-01 10:09:56 -0600

    Consideración de sectores
    se añadió al método la búsqueda y adición de los nuevos sectores como en su eliminación

--- Codigo/clases/mapa.py
+++ Codigo/clases/mapa.py
@@ -4,13 +4,17 @@ class mapa:
     nombre = str()
     ciudades = list()
     rutas = list()
-    sectores = 0
+    sectores = list()
     inicio = None
     
 
     def anadirCiudad(self,ciudad:ciudad):
         self.ciudades.append(ciudad)
-        
+        for sector in self.sectores():
+            if(ciudad.sector == sector)
+                return 0
+        self.sectores.append(ciudad.sector)
+        self.sectores.sort()
         
     def quitarCiudad(self,nombre: str):
         n = 0
@@ -20,3 +24,12 @@ class mapa:
                 break
             else:
                 n+=1
+   def purgarSectores(self,sector):
+        for ciudad in self.ciudades:
+            if(ciudad.sector == sector):
+                return 0
+        n = 0
+        for seccion in self.sectores():
+            if(seccion = sector)
+                self.sectores.pop(n)
+                return 1
