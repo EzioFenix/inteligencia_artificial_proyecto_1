ab28e9772253073678f33928f8bcaa86c4513e0f
BaudolinoII <30911202+BaudolinoII@users.noreply.github.com>
2022-03-01 10:13:33 -0600

    Consideraciones de sector
    se ha implementados a los métodos de añadir y quitar los sectores, a su vez añadiendo otra lista.

--- Codigo/clases/mapa.py
+++ Codigo/clases/mapa.py
@@ -11,7 +11,7 @@ class mapa:
     def anadirCiudad(self,ciudad:ciudad):
         self.ciudades.append(ciudad)
         for sector in self.sectores():
-            if(ciudad.sector == sector)
+            if(ciudad.seccion == sector)
                 return 0
         self.sectores.append(ciudad.sector)
         self.sectores.sort()
@@ -20,16 +20,20 @@ class mapa:
         n = 0
         for target in self.ciudades:
             if target.nombre == nombre:
+                sect = target.seccion
                 self.ciudades.pop(n)
+                purgarSectores(sect)
                 break
             else:
                 n+=1
    def purgarSectores(self,sector):
         for ciudad in self.ciudades:
-            if(ciudad.sector == sector):
+            if(ciudad.seccion == sector):
                 return 0
         n = 0
         for seccion in self.sectores():
             if(seccion = sector)
                 self.sectores.pop(n)
                 return 1
+            else
+                n += 1
